533775f803613e5078db06e1cc2120b423d68ef8
“KaitlynHolling” <kaitlynholling@gmail.com>
2023-03-09 22:17:47 -0500

    Ex7

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import { CheckBox } from '@rneui/themed';
+import { Button, Text, Input, ThemeProvider } from '@rneui/base';
 
-export default function App() {
+const TextInput = () => {
+  const [text, onChangeText] = React.useState('New Task');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder= "New Task"
+      />
+      <Button
+        title="Add"
+        onPress={() => (onChangeText(text))}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +31,51 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+<Stack row align="center" spacing={4}>
+  <CheckBox checked title="Do Homework" />
+  <CheckBox checked title="Feed Dog" />
+  <CheckBox checked title="Go Running" />
+  <CheckBox checked title="Call Grandma" />
+  
+</Stack>
+
+export default function App() {
+
+  let data = [
+    {
+      key: "1",
+      title: "Do Homework"
+    },
+    {
+      key: "2",
+      title: "Feed Dog"
+    },
+    {
+      key: "3",
+      title: "Go Running"
+    },
+    {
+      key: "4",
+      title: "Call Grandma"
+    },
+  ]
+
+  let renderItem = ({ item }) => {
+    return <Text>{item.title}</Text>
+  }
+  return (<SafeAreaView style={styles.container}>
+    <FlatList data={data} renderItem={renderItem}></FlatList>
+    </SafeAreaView>)
+}
+
+  return (
+    <View style={styles.container}>
+      <Text>To do: </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+
+
+  
+  
